1c5e4b042e3ce731fda2a8419d8b4d5629384e2e
GorINIch73 <ggorinich@gmail.com>
2025-05-13 17:13:04 +0300

    русская кодировака в PDF

--- src/PdfExporter.cpp
+++ src/PdfExporter.cpp
@@ -1,26 +1,54 @@
 #include "PdfExporter.h"
+#include <cstddef>
 #include <hpdf.h>
+#include <hpdf_font.h>
+#include <iostream>
 
-bool PdfExporter::ExportToPdf(const std::vector<std::map<std::string, std::string>>& data, const std::string& filename) {
+#include <codecvt>
+#include <locale>
+
+bool PdfExporter::ExportToPdf(
+    const std::vector<std::map<std::string, std::string>> &data,
+    const std::string &filename) {
     HPDF_Doc pdf = HPDF_New(nullptr, nullptr);
-    if (!pdf) return false;
+    if (!pdf)
+        return false;
 
     HPDF_Page page = HPDF_AddPage(pdf);
     HPDF_Page_SetSize(page, HPDF_PAGE_SIZE_A4, HPDF_PAGE_PORTRAIT);
 
+    // поддержка кирилицы
+    HPDF_UseUTFEncodings(pdf); // Включить UTF-8
+    HPDF_Font font = HPDF_GetFont(
+        pdf, HPDF_LoadTTFontFromFile(pdf, "NotoSans-Regular.ttf", HPDF_TRUE),
+        "UTF-8");
+
+    if (!font) {
+        std::cerr << "Ошибка: не удалось загрузить шрифт" << std::endl;
+        HPDF_Free(pdf);
+        return false;
+    }
+    // Устанавливаем шрифт и размер
+    HPDF_Page_SetFontAndSize(page, font, 14);
+
     HPDF_Page_BeginText(page);
     HPDF_Page_MoveTextPos(page, 50, HPDF_Page_GetHeight(page) - 50);
-    HPDF_Page_SetFontAndSize(page, HPDF_GetFont(pdf, "Helvetica", nullptr), 12);
+    // HPDF_Page_SetFontAndSize(page, HPDF_GetFont(pdf, "Helvetica", nullptr),
+    // 12);
+
+    // std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+    // std::string utf8Text = converter.to_bytes(L"Привет");
 
     // Заголовок
+    // HPDF_Page_ShowText(page, utf8Text);
     HPDF_Page_ShowText(page, "Отчет из базы данных");
     HPDF_Page_EndText(page);
 
     // Вывод данных
     float y = HPDF_Page_GetHeight(page) - 80;
-    for (const auto& row : data) {
+    for (const auto &row : data) {
         std::string line;
-        for (const auto& [key, value] : row) {
+        for (const auto &[key, value] : row) {
             line += key + ": " + value + " | ";
         }
         HPDF_Page_BeginText(page);
